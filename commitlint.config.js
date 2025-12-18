65f5023bc9cc1dbf1f6c38d1b4e2fe74f5c3aae4
zhonghuawei <1984974676@qq.com>
2025-12-18 11:21:35 +0800

    build(config): 更新 commitlint 配置规则

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,25 +1,44 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "revert",
+        "build",
+      ],
+    ],
     // 范围枚举类型
-    'scope-enum': [2, 'always', [
-      'api',
-      'components',
-      'pages',
-      'utils',
-      'styles',
-      'assets',
-      'config',
-      'docs',
-      'build'
-    ]],
-    // 主题长度
-    'header-max-length': [2, 'always', 100],
-    // 主题不能为空
-    'subject-empty': [2, 'never'],
-    // 主题必须以英文句号结尾
-    'subject-full-stop': [2, 'never', '.'],
-    // 主题必须以小写开头
-    'subject-case': [2, 'always', 'lower-case']
-  }
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "api",
+        "components",
+        "pages",
+        "utils",
+        "styles",
+        "assets",
+        "config",
+        "docs",
+        "build",
+      ],
+    ],
+    "type-case": [0],
+    "type-empty": [0],
+    "scope-empty": [0],
+    "scope-case": [0],
+    "subject-full-stop": [0, "never"],
+    "subject-case": [0, "never"],
+    "header-max-length": [0, "always", 72],
+  },
 };
